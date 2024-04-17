5f4768fbf21c3ddb2459c52ee9aeef9b9ed393ce
scivision <scivision@users.noreply.github.com>
2024-04-17 13:38:39 -0400

    cleanup

--- disassembly/dis_div.c
+++ disassembly/dis_div.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
-int main(){
+int main(void){
    volatile float a = (float)rand();
    float x = a / 2.;
+   return EXIT_SUCCESS;
 }
--- disassembly/dis_mult.c
+++ disassembly/dis_mult.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
-int main(){
+int main(void){
    volatile float a = (float)rand();
    float x = a * 0.5;
+   return EXIT_SUCCESS;
 }
